36b7ff0e8adfc238c6726dacc2eab74343a19cbc
20641118 <callehj@hotmail.se>
2019-08-25 16:02:57 +0200

    Check if a connection is already established

--- connect.js
+++ connect.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const uri = process.env.ADMIN_URI;
+if(!mongoose.connection){
+	const uri = process.env.MONGODB_URI;
 
-if(!uri)
-	throw new Error("Environment variable ADMIN_URI is not specified");
+	if(!uri)
+		throw new Error("Environment variable ADMIN_URI is not specified");
 
-// Connect to MongoDB
-mongoose.connect(uri, { useNewUrlParser: true })
-	.catch(err => {
-		console.log(err);
+	// Connect to MongoDB
+	mongoose.connect(uri, { useNewUrlParser: true })
+		.catch(err => {
+			console.log(err);
+		});
+
+	// Check for connection errors and confirm connection
+	const db = mongoose.connection;
+	db.on("error", console.error.bind(console, "connection error:"));
+	db.once("open", () => {
+		console.log("Connected to CMS database");
 	});
-
-// Check for connection errors and confirm connection
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-	console.log("Connected to CMS database");
-});
-
-module.exports = db;
+}else {
+	console.log("Already connected to a MongoDB");
+}
