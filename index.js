49159649d03ddbaa2c138073b5127f8ece122214
20641118 <callehj@hotmail.se>
2019-08-25 16:03:31 +0200

    Require connect.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const render = require("./controllers/render");
 const save = require("./controllers/save");
 const publish = require("./controllers/publish");
 const upload = require("./controllers/upload");
+require("./connect");
 
 module.exports = {
 	render,
